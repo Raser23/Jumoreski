67edde5eebb4575a00ef57484bab2753b2800c63
Neshchadim Sergey <ser.neshadim@yandex.ru>
2017-07-08 08:49:47 +0700

    HZ

--- index.py
+++ index.py
@@ -24,13 +24,15 @@ def send_anek(message):
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
+    print("/bot")
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
 @server.route("/")
 def webhook():
+    print("/")
     bot.remove_webhook()
-    bot.set_webhook(url=config.HOST)
+    bot.set_webhook(url=config.HOST +"/bot")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
