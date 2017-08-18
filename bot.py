a025a362ce084dd2353b77275b4a865fa1fe414a
Neshchadim Sergey <ser.neshadim@yandex.ru>
2017-08-18 18:35:37 +0700

    Faster, better, stronger

--- bot.py
+++ bot.py
@@ -34,12 +34,12 @@ def send_anek(message):
 @server.route("/bot", methods=['POST'])
 def getMessage():
     updates = [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))]
-    message =  updates[0]['message']
-    chat = message["chat"]
-    user = message['from_user']
+    message = updates[0]['message']
+    #chat = message["chat"]
+    #user = message['from_user']
     print(message)
-    print(chat)
-    print(user)
+    #print(chat)
+    #print(user)
     bot.process_new_updates(updates)
     return "ok", 200
 
