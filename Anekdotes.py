469b8b3dd45674b8b73347da620f38b600243fd8
Raser23 <ser.neshadim@yandex.ru>
2017-09-08 19:37:35 +0700

    ?

--- Anekdotes.py
+++ Anekdotes.py
@@ -53,7 +53,7 @@ import DB
 def get_random():
     return DB.get_random_anek()
 
-mdl = make_markov_model(DB.get_model("all_2")+DB.get_model("all_2"))
+mdl = make_markov_model(DB.get_model("all_1")+DB.get_model("all_2"))
 from markov.sentence_generator import generate_random_sentence
 
 def generate_anek(model_index):
